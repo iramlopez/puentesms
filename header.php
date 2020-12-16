4449fb09a406e1c6ed02e86d7832fcd39609dd4c
Iram <iram_lopez@yahoo.com>
2020-12-15 21:54:34 -0700

    Second Entry

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
               <li class="active"><a href="index.php">Home</a></li>
+			  <li><a href="goals.php">Goals</a><li>
               <li><a href="excersizelist.php">Excerises</a></li>
 			  <li><a href="nutrition.php">Nutricion</a></li>
               <li><a href="news.php">News</a></li>
